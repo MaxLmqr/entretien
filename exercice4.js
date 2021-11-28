ae7f409bbea7dc1855ff57ade9fdbffba982def6
MaxLmqr <max.lemasquerier@protonmail.com>
2021-11-28 19:54:37 +0100

    correction exercice 4

--- exercice4.js
+++ exercice4.js
@@ -4,13 +4,13 @@ import fetch from "node-fetch";
 const url =
   "https://www.immobilier.notaires.fr/pub-services/immodecret-stat1/v1/indicateurs?typeBien=AMA&nivGeo=COMMUNE&codeInsee=91425";
 
-const getCommunePrices = () => {
-  const response = fetch(url, {
+const getCommunePrices = async () => {
+  const response = await fetch(url, {
     headers: {
       Referer: "https://www.immobilier.notaires.fr/",
     },
   });
-  const data = response.json();
+  const data = await response.json();
   console.log(data);
 };
 
